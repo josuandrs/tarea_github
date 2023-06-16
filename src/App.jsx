4082c72e8354fa5f0bf124738321e341497849c5
Josue <jasovi@gmail.com>
2023-06-16 16:11:19 -0500

    primer avance

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Tarjeta from './Tarjeta'
+
+function App() {
+
+  return (
+    <>
+      <Tarjeta />
+    </>
+  )
+}
+
+export default App
---
+++ src/Tarjeta.jsx
@@ -0,0 +1,9 @@
+import './Tarjeta.css'
+
+function Tarjeta() {
+  return (
+    <div>Tarjeta</div>
+  )
+}
+
+export default Tarjeta
